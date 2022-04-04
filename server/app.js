6004336d0891ae92fda10f6668a6b1610d68cfff
Gianni P <perkinsgianni@gmail.com>
2022-04-04 16:58:06 -0400

    Update code in app.js to pass all tests

--- server/app.js
+++ server/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const fs = require('fs'); // fs module enables interaction with file system
 const app = express();
-const morgan = require('morgan');
-const csv = require('csv-parser');
+// const csv = require('csv-parser');
+
+// create log headers
+let log = ["Agent","Time","Method","Resource","Version","Status"];
+
+// create file called log.csv and write headers to file
+fs.writeFile("logs/log.csv", log + "\n", err => {
+    if (err) {
+        console.log(err);
+    }
+});
 
 // logging middleware
 app.use((req, res, next) => {
     // write your logging code here
-    let nativeAgent = req.get('User-Agent');
-    let agent = nativeAgent.replace(',', '') ;
-    let nativeTime = new Date();
-    let time = nativeTime.toISOString();
+    let agent = req.get('User-Agent').replace(',', '') ;
+    let time = new Date().toISOString();
     let method = req.method;
     let resource = req.url;
     let version = "HTTP/" + req.httpVersion;
@@ -18,16 +25,17 @@ app.use((req, res, next) => {
 
     // format log line
     let logText = `${agent},${time},${method},${resource},${version},${status}`;
+    console.log(logText);
 
     // save log in csv file
-    fs.appendFile('log.csv', logText + "\n", err => {
+    fs.appendFile('logs/log.csv', logText + "\n", err => {
         if (err) {
             console.log(err);
         }
     });
 
     // open csv file as readable stream
-    // fs.createReadStream('log.csv')
+    // fs.createReadStream('logs/log.csv')
     //     .pipe(csv())
     //     .on('data', (row) => {
     //         console.log(row);
@@ -39,18 +47,15 @@ app.use((req, res, next) => {
     next();
 });
 
-// morgan dev middleware
-app.use(morgan('dev'));
-
 // routes
 app.get('/', (req, res) => {
     // write your code to respond "ok" here
-    res.sendStatus(200).send(console.log())
+    res.sendStatus(200)
 });
 
 app.get('/logs', (req, res) => {
     // write your code to return a json object containing the log data here
-    fs.readFile('./log.csv', (err, data) => {
+    fs.readFile('logs/log.csv', (err, data) => {
         // create new arr
         let jsonArr = [];
         // convert data object to str, split at each new line, return array of str
@@ -59,16 +64,16 @@ app.get('/logs', (req, res) => {
         let logHeaders = logStrings[0].split(",");
 
         // iterate through arr after headers
-        for ( let i = 1; i < logStrings.length; i++ ) {
+        for ( let i = 1; i < logStrings.length - 1; i++ ) {
             // create new obj
             let jsonObj = {};
-            // for each header, invoke prop and index
-            logHeaders.forEach((prop, index) => {
+            // iterate through headers, invoke prop (element) and its index
+            logHeaders.map((prop, index) => {
                 // split and index str at each comma, assign as obj prop
                 jsonObj[prop] = logStrings[i].split(",")[index];
             });
             // push obj into arr
-            jsonArr.push(jsonObj)
+            jsonArr.push(jsonObj);
         }
         // send arr as respone to /logs req
         res.send(jsonArr);
